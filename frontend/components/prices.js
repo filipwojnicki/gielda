69093e3f4cf8cc16a3105225751b9a300810d0c0
Filip Wojnicki <filip.wojnicki@op.pl>
2019-11-19 00:34:03 +0100

    add stock prices template, websocket component

---
+++ frontend/components/prices.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import { Table, Spinner } from 'reactstrap'
+
+export default class prices extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="p-4">Stock prices</h2>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Value</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.prices.Items ? this.props.prices.Items.map(item =>
+              <tr>
+                <td>{item.Name}</td>
+                <td>{item.Price}</td>
+              </tr>
+            ) :
+            <Spinner className="mx-auto" size="lg" color="secondary" />
+            }
+          </tbody>
+        </Table>
+
+      </div>
+    )
+  }
+}
---
+++ frontend/components/webSocket.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+export default class webSocket extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ws: null
+    };
+  }
+
+  componentDidMount() {
+    this.connect();
+  }
+
+  timeout = 250;
+
+  connect = () => {
+    var ws = new WebSocket("ws://localhost:8080");
+    let that = this;
+    var connectInterval;
+
+    ws.onopen = () => {
+      console.log("connected websocket");
+
+      this.setState({
+        ws: ws
+      });
+
+      that.timeout = 250;
+      clearTimeout(connectInterval);
+    };
+
+    ws.onclose = e => {
+      this.props.onMessage(null);
+
+      that.timeout = that.timeout + that.timeout;
+      connectInterval = setTimeout(this.check, Math.min(10000, that.timeout));
+    };
+
+    ws.onerror = err => {
+      this.props.onMessage(null);
+
+      ws.close();
+    };
+
+    ws.onmessage = data => {
+      if (!data) return;
+      this.props.onMessage(data);
+    }
+  };
+
+  check = () => {
+    const { ws } = this.state;
+
+    if (!ws || ws.readyState == WebSocket.CLOSED) this.connect();
+  };
+
+  render() {
+    return(null);
+  }
+}
