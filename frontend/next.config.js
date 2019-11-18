7e3013be245384c8ee52e673486ab2bb4f4b96f6
Filip Wojnicki <filip.wojnicki@op.pl>
2019-11-19 00:32:29 +0100

    add css import support to next.js config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,17 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+const withPlugins = require('next-compose-plugins')
+const sass = require('@zeit/next-sass')
+const css = require('@zeit/next-css')
 
-    return config
-  }
-}
+module.exports = withPlugins([
+    [css],
+    [sass]
+  ], {
+    webpack: config => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty'
+      }
+
+      return config
+    }
+});
